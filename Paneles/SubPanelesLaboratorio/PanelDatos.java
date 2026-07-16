f4134fb5772687cd51c7445c54d92f8cd62a27a8
Juansebastianflorescordova-Ops <juansebastianflorescordova@gmail.com>
2026-07-16 01:10:08 -0500

    actualizando

--- Paneles/SubPanelesLaboratorio/PanelDatos.java
+++ Paneles/SubPanelesLaboratorio/PanelDatos.java
@@ -5,10 +5,18 @@
     public class PanelDatos extends JPanel {
         public PanelDatos() {
             configurarPanel();
+            cargarComp(); 
         }
 
         public void configurarPanel() {
             setLayout(new FlowLayout());
-            setBorder(BorderFactory.createTitledBorder("Datos Generados"));
+            setBorder(BorderFactory.createTitledBorder("Generacion o ingreso de datos"));
+            setPreferredSize(new Dimension(300, 0));
+        }
+        public void cargarComp(){
+            JTextField caja; 
+            caja  = new JTextField(20); 
+            add(caja);
+
         }
     }
