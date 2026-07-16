8cfbf7bc22924029f9ef5f39841f6dd1a3d86748
KZeTa <kztch19@gmail.com>
2026-07-16 11:17:15 -0500

    Agregando listener a botones y configurando generación aleatoria de valores

--- Paneles/SubPanelesLaboratorio/PanelGrafico.java
+++ Paneles/SubPanelesLaboratorio/PanelGrafico.java
@@ -8,4 +8,26 @@ public class PanelGrafico extends JPanel {
         JTextArea area = new JTextArea("ddadadada");
         add(area); 
     }  
+
+    int [] datos;
+
+    public int[] generarDatosAleatorio(int cantidad){
+        datos = new int[cantidad];
+        int i=0;
+        do{
+            datos[i] = (int)(Math.random()*50);
+            if (datos[i]!=0) {
+                i++;
+            }   
+        }while(i< cantidad);
+        return datos;
+    }
+
+    public int[] getDatos(){
+        return datos;
+    }
+
+    public void setDatos(int[] datos){
+        this.datos=datos;
+    }
 }
