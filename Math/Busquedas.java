c98e6711a72f3628eb253f5e39916e6abc746cbe
Juansebastianflorescordova-Ops <juansebastianflorescordova@gmail.com>
2026-07-15 23:25:32 -0500

    package math

--- Math/Busquedas.java
+++ Math/Busquedas.java
@@ -2,4 +2,33 @@ package Math;
 
 public class Busquedas {
     
+
+
+
+    public int busquedaSecuencial(int[] arreglo, int valorBuscado) {
+    for (int i = 0; i < arreglo.length; i++) {
+        if (arreglo[i] == valorBuscado) {
+            return i; // Retorna la posición donde se encontró
+        }
+    }
+    return -1; // No se encontró el valor
+    }
+    public int busquedaBinaria(int[] arreglo, int valorBuscado) {
+        int izquierda = 0;
+        int derecha = arreglo.length - 1;
+
+        while (izquierda <= derecha) {
+            int centro = (izquierda + derecha) / 2;
+
+            if (arreglo[centro] == valorBuscado) {
+            return centro; // Se encontró
+            } else if (valorBuscado < arreglo[centro]) {
+            derecha = centro - 1;
+            } else {
+            izquierda = centro + 1;
+            }
+        }
+
+        return -1; // No se encontró
+    }
 }
